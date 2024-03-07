e457de26b31c41122b6663be1c784b966afcec6d
lishing <shingli.chan@gmail.com>
2024-03-07 12:40:11 +0800

    msf-usage-metrics'

---
+++ app.py
@@ -0,0 +1,93 @@
+import csv
+import boto3
+import pandas as pd
+import streamlit as st
+import tempfile
+from st_files_connection import FilesConnection
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+# Main Streamlit app
+def main():
+
+    st.header('MSF Usage Metrics')
+
+    conn = st.connection('s3', type=FilesConnection)
+    
+    try: 
+        with st.spinner('Downloading CSV file...'):
+            df = conn.read("dev-app-logging/msf-report/msf_logs_csv//output.csv", input_format="csv", ttl=60)
+
+            # metric 1: total usage
+            total = df["version"].count()
+            time_saved = f'{round(total * 12.5 / 60)} hours'
+            
+            # metric 2: get usage of the month
+            df['Date'] = pd.to_datetime(df['Date'])
+            df['Date_Only'] = df['Date'].dt.strftime('%Y-%m-%d')
+            today = datetime.today().date()
+            df_today = df[df['Date'].dt.date == today]
+            current_month = today.month
+            current_year = today.year
+            df_this_month = df[(df['Date'].dt.month == current_month) & (df['Date'].dt.year == current_year)]
+            usage_count_this_month = df_this_month.shape[0]
+
+            # Calculate usage count for the previous month
+            previous_month = current_month - 1 if current_month > 1 else 12
+            previous_year = current_year if current_month > 1 else current_year - 1
+            df_previous_month = df[(df['Date'].dt.month == previous_month) & (df['Date'].dt.year == previous_year)]
+            usage_count_previous_month = df_previous_month.shape[0]
+
+            # Calculate delta between current month and previous month
+            delta = usage_count_this_month - usage_count_previous_month
+            if usage_count_previous_month != 0:
+                percentage_delta = ((usage_count_this_month - usage_count_previous_month) / usage_count_previous_month) * 100
+            else:
+                percentage_delta = 0
+
+            # metric 3: group by assessment type and count maximum
+            assessment_type_counts = df['assessment_type'].value_counts()
+            most_used_assessment_types = assessment_type_counts.idxmax()
+            most_used_count = assessment_type_counts.max()
+
+            # create 2 columns to hold the metric
+            # add a column on the time saved, which is 12.5 mins. will assume 
+            col1, col2, col3, col4 = st.columns(4)
+
+            col1.metric(label = 'Total Usage to date', value=total, help = 'Data includes usage since Oct 2023, and will be refreshed every early part of the week')
+            col3.metric(label = 'Usage this month', value=usage_count_this_month, delta=delta, help='Delta is calculated based MoM usage')
+            col4.metric(label = 'Most utilized Header', value=most_used_assessment_types, help='Headers include Family, Employment, Shelter, Health, Financial Support, Food')
+            col2.metric(label = 'Total time saved to date', value=time_saved, help='Assume each usage can save 12.5 minutes per SA report. Based on users\' study, done in Nov 2023')
+
+            # Aggregate usage by month
+            df['year_month'] = df['Date'].dt.to_period('M')
+            usage_by_month = df.groupby('year_month').size().cumsum()
+
+            # Plot aggregate usage over time
+            fig, ax = plt.subplots()
+            ax.plot(usage_by_month.index.astype(str), usage_by_month.values, marker='o')
+            ax.set_title('Cumulative Usage Over Time')
+            ax.set_xlabel('Month')
+            ax.set_ylabel('Number of generated reports')
+            ax.grid(False)
+
+            # Display the plot using st.pyplot
+            st.pyplot(fig)
+
+            st.subheader('Raw data')
+            st.dataframe(df, column_config={
+                "Date_Only": "Date",
+                "version": None,
+                "assessment_type": "Assessment Type",
+                "input_note": "Input",
+                "output_note": "Generated Output",
+                "year_month": None,
+            },
+            hide_index=True)
+            
+    except pd.errors.ParserError as e:
+        st.error(f"Error parsing CSV file: {e}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ lambda_function.py
@@ -0,0 +1,106 @@
+import os
+import json
+import csv
+import boto3
+import pandas as pd
+import tempfile  
+import re
+from datetime import datetime
+
+s3 = boto3.client('s3')
+
+# Function to extract and format date from filename
+def extract_date(filename):
+    match = re.search(r'\d{4}-\d{2}-\d{2}', filename)
+    
+    if match:
+        date_str = match.group()
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        formatted_date = date_obj.strftime('%Y-%m-%d')
+        return formatted_date
+    else:
+        return None
+
+# Function to download JSON files from S3 bucket
+def download_json_files(bucket_name, folder_prefix, temp_dir):
+    file_keys = []
+    paginator = s3.get_paginator('list_objects_v2')
+
+    for page in paginator.paginate(Bucket=bucket_name, Prefix=folder_prefix):
+        for obj in page.get('Contents', []):
+            full_file_name = obj["Key"].split("/")[-1]
+            file_name = os.path.join(temp_dir, full_file_name)
+            s3.download_file(bucket_name, obj['Key'], file_name)
+            file_keys.append(full_file_name)
+
+    return [os.path.join(temp_dir, key) for key in file_keys]
+
+# Function to flatten JSON data
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+# Function to convert JSON files to CSV
+def json_to_csv(json_files, csv_file):
+    with open(csv_file, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        header_written = False
+        for json_file in json_files:
+            with open(json_file, 'r') as f:
+                try:
+                    data = json.load(f)
+                    if isinstance(data, dict):
+                        record = flatten_json(data)
+
+                        selected_keys = list(record.keys())[:4]
+
+                        if not header_written:
+                            csv_writer.writerow(['Date'] + selected_keys) # Header
+                            header_written = True
+                        file_date = extract_date(json_file)
+                        if file_date:
+                            selected_values = [record[key] for key in selected_keys]
+                            csv_writer.writerow([file_date] + selected_values) # values
+                except json.JSONDecodeError as e:
+                    print(f"Error parsing JSON file '{json_file}': {e}")
+                    # Handle the error as needed, e.g., skip the file or log the error
+
+    print(f"CSV file '{csv_file}' created successfully.")
+
+# Function to upload CSV file to S3 bucket
+def upload_csv_to_s3(csv_file, bucket_name, folder_prefix):
+    s3.upload_file(csv_file, bucket_name, f"{folder_prefix}/{os.path.basename(csv_file)}")
+
+# Lambda handler function
+def lambda_handler(event, context):
+    source_bucket_name = 'dev-app-logging'
+    source_folder_prefix = 'msf-report/default_logs/'
+    destination_bucket_name = 'dev-app-logging'
+    destination_folder_prefix = 'msf-report/msf_logs_csv/'
+    
+    temp_dir = tempfile.mkdtemp()
+    json_files = download_json_files(source_bucket_name, source_folder_prefix, temp_dir)
+    csv_file = os.path.join(temp_dir, 'output.csv')
+    json_to_csv(json_files, csv_file)
+    
+    # Upload CSV file to another S3 bucket
+    upload_csv_to_s3(csv_file, destination_bucket_name, destination_folder_prefix)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('CSV file created and uploaded successfully.')
+    }
