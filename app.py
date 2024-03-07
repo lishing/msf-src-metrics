280acd348247ee859bf5181d6dd917cf3451624a
lishing <shingli.chan@gmail.com>
2024-03-07 15:31:36 +0800

    added date range, fixed headers, and display raw data in descending order

--- app.py
+++ app.py
@@ -18,72 +18,84 @@ def main():
         with st.spinner('Downloading CSV file...'):
             df = conn.read("dev-app-logging/msf-report/msf_logs_csv//output.csv", input_format="csv", ttl=60)
 
-            # metric 1: total usage
-            total = df["version"].count()
-            time_saved = f'{round(total * 12.5 / 60)} hours'
-            
-            # metric 2: get usage of the month
+            # Find the earliest and latest dates in the 'Date' column
             df['Date'] = pd.to_datetime(df['Date'])
             df['Date_Only'] = df['Date'].dt.strftime('%Y-%m-%d')
-            today = datetime.today().date()
-            df_today = df[df['Date'].dt.date == today]
-            current_month = today.month
-            current_year = today.year
-            df_this_month = df[(df['Date'].dt.month == current_month) & (df['Date'].dt.year == current_year)]
-            usage_count_this_month = df_this_month.shape[0]
+            earliest_date = df['Date'].min()
+            latest_date = df['Date'].max()
 
-            # Calculate usage count for the previous month
-            previous_month = current_month - 1 if current_month > 1 else 12
-            previous_year = current_year if current_month > 1 else current_year - 1
-            df_previous_month = df[(df['Date'].dt.month == previous_month) & (df['Date'].dt.year == previous_year)]
-            usage_count_previous_month = df_previous_month.shape[0]
+            selected_date_range = st.date_input(
+                "Select Date Range for the report",
+                (earliest_date.date(), latest_date.date()) # detault value
+            )
 
-            # Calculate delta between current month and previous month
-            delta = usage_count_this_month - usage_count_previous_month
-            if usage_count_previous_month != 0:
-                percentage_delta = ((usage_count_this_month - usage_count_previous_month) / usage_count_previous_month) * 100
-            else:
-                percentage_delta = 0
+            if selected_date_range:
+                start_date, end_date = selected_date_range
+                df_selected = df[(df['Date'].dt.date >= start_date) & (df['Date'].dt.date <= end_date)]
 
-            # metric 3: group by assessment type and count maximum
-            assessment_type_counts = df['assessment_type'].value_counts()
-            most_used_assessment_types = assessment_type_counts.idxmax()
-            most_used_count = assessment_type_counts.max()
+                # metric 1: total usage
+                total = df_selected["version"].count()
 
-            # create 2 columns to hold the metric
-            # add a column on the time saved, which is 12.5 mins. will assume 
-            col1, col2, col3, col4 = st.columns(4)
+                # metric 2: total time saved
+                time_saved = f'{round(total * 12.5 / 60)} hours'
+                
+                # metric 3: Usage count of this month, with MoM growth
+                today = datetime.today().date()
+                df_today = df[df['Date'].dt.date == today]
+                current_month = today.month
+                current_year = today.year
+                df_this_month = df[(df['Date'].dt.month == current_month) & (df['Date'].dt.year == current_year)]
+                usage_count_this_month = df_this_month.shape[0]
 
-            col1.metric(label = 'Total Usage to date', value=total, help = 'Data includes usage since Oct 2023, and will be refreshed every early part of the week')
-            col3.metric(label = 'Usage this month', value=usage_count_this_month, delta=delta, help='Delta is calculated based MoM usage')
-            col4.metric(label = 'Most utilized Header', value=most_used_assessment_types, help='Headers include Family, Employment, Shelter, Health, Financial Support, Food')
-            col2.metric(label = 'Total time saved to date', value=time_saved, help='Assume each usage can save 12.5 minutes per SA report. Based on users\' study, done in Nov 2023')
+                # Calculate usage count for the previous month
+                previous_month = current_month - 1 if current_month > 1 else 12
+                previous_year = current_year if current_month > 1 else current_year - 1
+                df_previous_month = df[(df['Date'].dt.month == previous_month) & (df['Date'].dt.year == previous_year)]
+                usage_count_previous_month = df_previous_month.shape[0]
 
-            # Aggregate usage by month
-            df['year_month'] = df['Date'].dt.to_period('M')
-            usage_by_month = df.groupby('year_month').size().cumsum()
+                # Calculate delta between current month and previous month
+                delta = usage_count_this_month - usage_count_previous_month
+                if usage_count_previous_month != 0:
+                    percentage_delta = ((usage_count_this_month - usage_count_previous_month) / usage_count_previous_month) * 100
+                else:
+                    percentage_delta = 0
 
-            # Plot aggregate usage over time
-            fig, ax = plt.subplots()
-            ax.plot(usage_by_month.index.astype(str), usage_by_month.values, marker='o')
-            ax.set_title('Cumulative Usage Over Time')
-            ax.set_xlabel('Month')
-            ax.set_ylabel('Number of generated reports')
-            ax.grid(False)
+                # metric 4: group by assessment type and count maximum
+                assessment_type_counts = df_selected['assessment_type'].value_counts()
+                most_used_assessment_types = assessment_type_counts.idxmax()
+                most_used_count = assessment_type_counts.max()
 
-            # Display the plot using st.pyplot
-            st.pyplot(fig)
+                # metrics column
+                col1, col2, col3, col4 = st.columns(4)
 
-            st.subheader('Raw data')
-            st.dataframe(df, column_config={
-                "Date_Only": "Date",
-                "version": None,
-                "assessment_type": "Assessment Type",
-                "input_note": "Input",
-                "output_note": "Generated Output",
-                "year_month": None,
-            },
-            hide_index=True)
+                col1.metric(label = 'Total Usage to date', value=total, help = 'Data includes inputs submitted. Figures will be refreshed weekly.')
+                col2.metric(label = 'Total time saved to date', value=time_saved, help='Each usage is estimated to be able to save 12.5 minutes per SA report. Based on users\' study done in Nov 2023.')
+                col3.metric(label = 'Usage this month', value=usage_count_this_month, delta=delta, help='Delta is calculated based on MoM growth of the month today, not affected by time range.')
+                col4.metric(label = 'Most utilized Header', value=most_used_assessment_types, help='Headers include Family, Employment, Shelter, Health, Financial Support, Food.')
+
+                # Aggregate usage by month
+                df_selected['year_month'] = df_selected['Date'].dt.to_period('M')
+                usage_by_month = df_selected.groupby('year_month').size().cumsum()
+
+                # Plot aggregate usage over time
+                fig, ax = plt.subplots()
+                ax.plot(usage_by_month.index.astype(str), usage_by_month.values, marker='o')
+                ax.set_title('Cumulative Usage Over Time')
+                ax.set_xlabel('Month')
+                ax.set_ylabel('Number of generated reports')
+                ax.grid(False)
+                st.pyplot(fig)
+
+                st.subheader('Raw data')
+                st.dataframe(df_selected.sort_values(by='Date_Only', ascending=False), column_config={
+                    "Date_Only": "Date",
+                    "version": None,
+                    "assessment_type": "Assessment Type",
+                    "input_note": "Input",
+                    "output_note": "Generated Output",
+                    "year_month": None,
+                },
+                hide_index=True)
             
     except pd.errors.ParserError as e:
         st.error(f"Error parsing CSV file: {e}")
@@ -91,3 +103,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
